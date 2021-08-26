9e40400a7e38afe4cf86ea7654f8f871e2472336
jim <jimc@bethel.com.tw>
2021-08-26 08:57:59 +0800

    2021-08-26

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My Tube',
+      // debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
--- lib/player.dart
+++ lib/player.dart
@@ -41,15 +41,104 @@ class _PlayerState extends State<Player> {
   
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: new BoxDecoration(color: Colors.white),
-      width: double.infinity,
-      child: Column(children: [
-        Text(download.title),
-        Text(download.author),
-        Text(download.duration.inSeconds.toString()),
-        Text(processing.toString())
-      ],)
+    return Material(
+      child: Container(
+        decoration: new BoxDecoration(color: Colors.white),
+        padding: EdgeInsets.all(20.0), //容器内补白
+        width: double.infinity,
+        child: download == null || download.title.length == 0 ? loading() : step2()
+      )
+    );
+  }
+
+  Widget loading() {
+    return new Center( //保证控件居中效果
+        child: new SizedBox(
+          width: 250.0,
+          height: 120.0,
+          child: new Container(
+            decoration: ShapeDecoration(
+              color: Color(0xffffffff),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8.0)),
+              ),
+            ),
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                new CircularProgressIndicator(),
+                // new Padding(
+                //   padding: const EdgeInsets.only(top: 20.0),
+                //   child: "loading",
+                // ),
+              ],
+            ),
+          ),
+        ),
+      );
+  }
+
+  Widget step2(){
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if(processing == 100)
+          Expanded(flex: 1, 
+            child: Text("WebView",
+              textAlign: TextAlign.left,
+              style: new TextStyle(
+                // color: Colors.blue,
+                fontSize: 20,
+              )
+            )
+          ),
+        Text(download.title,
+          textAlign: TextAlign.left,
+          style: new TextStyle(
+            color: Colors.blue,
+            fontSize: 25,
+          )
+        ),
+        Text("作者：" + download.author,
+          textAlign: TextAlign.left,
+          style: new TextStyle(
+            // color: Colors.blue,
+            fontSize: 20,
+          )
+        ),
+        if(processing < 100)
+          Text("時間：" + download.duration.toString().replaceAll(".000000", ""),
+            textAlign: TextAlign.left,
+            style: new TextStyle(
+              // color: Colors.blue,
+              fontSize: 20,
+            )
+          ),
+        if(processing < 100)
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(height: 30,),
+            LinearProgressIndicator(  
+                // backgroundColor: Colors.cyanAccent,  
+                // valueColor: new AlwaysStoppedAnimation<Color>(Colors.red),  
+                backgroundColor: Colors.grey[200],
+                valueColor: AlwaysStoppedAnimation(Colors.blue),
+                value: processing.toDouble() / 100,  
+              ),
+              Text(processing.toString(),
+                textAlign: TextAlign.center,
+                style: new TextStyle(
+                  // color: Colors.blue,
+                  fontSize: 20,
+                )
+              ),
+          ]
+        ),
+      ]
     );
   }
 }
